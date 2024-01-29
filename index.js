d68fb5ff40b6c83287489186a4155f1f6d8038ec
מוני ברוך <monybaruch@gmail.com>
2024-01-29 16:24:06 +0200

    adding a CAKE_RESTOCKED action

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const createStore = redux.createStore;
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+
+//action creator
+
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
   };
 }
 
@@ -25,6 +36,12 @@ const reducer = (state = initialState, action) => {
         numOfCakes: state.numOfCakes - 1,
       };
 
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+
     default:
       return state;
   }
@@ -39,5 +56,7 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+
+store.dispatch(restockCake(3));
 
 unsubscribe();
